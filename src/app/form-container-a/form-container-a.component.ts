e3a5b3d2c6b2124360c27ac3180586d10cc07ca8
jyotirmoy.p@eccentricengine.com <jyotirmoypal077@gmail.com>
2024-02-19 10:53:55 +0530

    Json redefined

--- src/app/form-container-a/form-container-a.component.ts
+++ src/app/form-container-a/form-container-a.component.ts
@@ -20,42 +20,11 @@ export class FormContainerAComponent {
   async ngOnInit() {
     await this.getData().subscribe((data :any) => {
       console.log(data);
-      // Do something with the JSON data
       this.selectedFormData = data?.formDetails?.find((el:any)=>
       {
         return el?.form_id == this.selectedFormId
       })
-      console.log(this.selectedFormData)    
-      let formGroup: Record<string, any> = {};
-      this.formStructure = this.selectedFormData?.form_details
-      console.log(this.formStructure)
-      this.formStructure.forEach((control) => {
-        console.log(control)
-        let controlValidators: Validators[] = [];
-  
-        if (control.validations) {
-          control.validations.forEach(
-            (validation: {
-              name: string;
-              validator: string;
-              message: string;
-            }) => {
-              if (validation.name === 'required'){
-                controlValidators.push(Validators.required);
-  
-              }
-              if(validation.name === 'pattern'){
-                controlValidators.push(Validators.pattern(validation?.validator))
-              }
-            }
-          );
-        }
-  
-        formGroup[control.param_name] = [control.value || '', controlValidators];
-      });
-      this.dynamicForm = this.fb.group(formGroup);
-      console.log(formGroup)
-      console.log(this.dynamicForm)
+      this.formControlWrapper()
     });
   }
   getErrorMessage(control: any) {
@@ -80,5 +49,36 @@ export class FormContainerAComponent {
       return;
     }
     console.log(this.dynamicForm.value);
+  }
+  formControlWrapper(){
+    console.log(this.selectedFormData)    
+    let formGroup: Record<string, any> = {};
+    this.formStructure = this.selectedFormData?.form_details
+    console.log(this.formStructure)
+    this.formStructure.forEach((control) => {
+      console.log(control)
+      let controlValidators: Validators[] = [];
+      if (control.validations) {
+        control.validations.forEach(
+          (validation: {
+            name: string;
+            validator: string;
+            message: string;
+          }) => {
+            if (validation.name === 'required'){
+              controlValidators.push(Validators.required);
+
+            }
+            if(validation.name === 'pattern'){
+              controlValidators.push(Validators.pattern(validation?.validator))
+            }
+          }
+        );
+      }
+      formGroup[control.param_name] = [control.value || '', controlValidators];
+    });
+    this.dynamicForm = this.fb.group(formGroup);
+    console.log(formGroup)
+    console.log(this.dynamicForm)
   }
 }
